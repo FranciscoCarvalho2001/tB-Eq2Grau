3ebc2fff2facefe453911fead2d286ed83f0ffac
FranciscoCarvalho2001 <101589943+FranciscoCarvalho2001@users.noreply.github.com>
2023-03-30 14:36:31 +0100

    commit final

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -13,8 +13,32 @@ namespace WebApplication1.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(int A, int B,int C )
         {
+            //var auxiliar
+            double delta = (B * B) - (4 * A * C);
+            string x1 = "", x2 = "";
+
+            //validar se há condições para efetuar o cálculo
+            if (A != 0)
+            {
+                if (delta >= 0)
+                {
+                    // x1= (-b + sqrt(delta))/(2*a)
+                    // x2= (-b - sqrt(delta))/(2*a)
+                    x1 = (-B + Math.Sqrt(delta) / (2 * A)) + "";
+                    x2 = (-B - Math.Sqrt(delta) / (2 * A)) + "";
+                }
+                else
+                {
+                    //solução completa
+                    x1 = -B / (2 * A) + " + " + Math.Sqrt(-delta) / (2 * A)+ " i";
+                    x2 = -B / (2 * A) + " - " + Math.Sqrt(-delta) / (2 * A) + " i";
+                }
+                //preparar os dados a serem enviados para o browser
+                ViewBag.X1 = x1;
+                ViewBag.X2 = x2;
+            }
             return View();
         }
 
